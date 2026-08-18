0ae687cf721270ae0eced3cb93a21b690bc8451c
muchu1006 <alejandro0610.gp@gmail.com>
2026-08-18 04:27:16 -0400

    feat: localStorage

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,11 +1,14 @@
-import { useState } from "react";
 import type { User } from "../models/users";
 import { getUsers } from "../services/getUsers";
 import { toast } from "sonner";
 import { useFetch } from "./useFetch";
+import { useStateLocalStorage } from "./useStateLocalStorage";
 
 export const useUser = () => {
-  const [initialStateUser, setInitialStateUser] = useState<User[]>([]);
+  const [initialStateUser, setInitialStateUser] = useStateLocalStorage<User[]>({
+    key: "users-crud",
+    initialValue: [],
+  });
   const { loading, error } = useFetch({
     service: getUsers,
     state: initialStateUser,
